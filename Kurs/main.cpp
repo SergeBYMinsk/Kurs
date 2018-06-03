bfffc9cad444cf526a0a42db1d3eb33a56e24b23
Сергей Стерликов <sterlikovsergey@icloud.com>
2018-06-03 16:57:24 +0300

    Initial Commit

---
+++ Kurs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Kurs
+//
+//  Created by Sergey Sterlikov on 03.06.2018.
+//  Copyright © 2018 Sergey Sterlikov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
